3a314d05810eba780249e5be394f33b134060ece
Almstedt <lena.targaryen20.06@gmail.com>
2021-06-25 19:42:51 +0200

    Isch bin TimeMaster

--- Aufgabe 1/PubMed.py
+++ Aufgabe 1/PubMed.py
@@ -2,9 +2,12 @@
 # with urllib.request.urlopen("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=27708327") as url:
 #     data = json.loads(url.read().decode())
 #     print(data)
-import pandas
+import pandas, datetime
+start = datetime.datetime.now()
 IDs = [31452104, 31437182, 31455877, 31535994]
 for element in IDs:
     data = pandas.read_json("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=" + str(element))
     print("Titel:", data["result"][str(element)]["title"], "\nPublikationsdatum:", data["result"][str(element)]["pubdate"], "\n")
-    
+ende = datetime.datetime.now()
+print("Dauer", ende-start, "Sekunden")
+print("Gesucht am:", start.strftime("%d.%m.%y %H:%M:%S"))
--- Aufgabe 1/README.md
+++ Aufgabe 1/README.md
@@ -5,14 +5,15 @@
 VS Code
 
 ## 2. Eine Python-Bibliothek
--Antwort-
+datetime2
+Mit dieser Bibliothek kann man Zeitstempel generieren lassen. Ich hab mir damit die Laufzeit des Programms ausgeben lassen (hat nicht lange gedauert). Zudem habe ich mir das Datum anzeigen lassen an dem ich das Programm ausgeführt habe. 
 
 ## 3. Eine Fehlermeldung und Ihre Lösung
 TypeError: can only concatenate str (not "int") to str 
 Lösung: in der url ein str vor IDs einfügen str(IDs[0])
 
 ## 4. Was ist JupyterLab?
--Antwort- 
+ Es ist die Evolution von Jupiter Notebook, mehr, neuer, besser und online
 
 ## 5. Was ist der große Unterschied zwischen den Webframeworks flask und Django
 -Antwort-
